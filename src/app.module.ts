b755205a8d6c0a760bfd46f6b48e79a717923b6e
rafael <rafaelromero938@gmail.com>
2022-12-23 17:46:24 -0600

    correccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { APP_FILTER } from '@nestjs/core';
 import { getEnvPath } from './common/helper/env.helper';
 import { AllExceptionsFilter } from './core/exception.interceptor';
 // import { DbconfigModule } from './modules/dbconfig/dbconfig.module';
+import {AppService } from './app.service';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/env/messages`);
 
@@ -15,6 +16,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/env/messages`);
     /*DbconfigModule*/],
   providers: [{ provide: APP_FILTER, useClass: AllExceptionsFilter }],
   controllers: [],
+  exports: [AppService],
 
 
 })
