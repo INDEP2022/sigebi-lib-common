672934ee511f5f7e200d154c3686dc3ece2489ce
rafael <rafaelromero938@gmail.com>
2023-01-05 14:22:52 -0600

    correccion de respuesta

--- src/core/exception.interceptor.ts
+++ src/core/exception.interceptor.ts
@@ -46,6 +46,7 @@ export class AllExceptionsFilter<T> implements ExceptionFilter {
     if (!Array.isArray(message)){
       message =[message];
     }
+    console.log(message);
     response.status(statusCode).json({
       //error: {
       //timestamp: new Date().toISOString(),
@@ -58,41 +59,34 @@ export class AllExceptionsFilter<T> implements ExceptionFilter {
   }
 }
 
-/*
-@Catch(HttpException)
-export class AllExceptionsFilter implements ExceptionFilter {
-  async catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const context = host.switchToHttp();
-    const response = context.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+// import {
+//   ExceptionFilter,
+//   Catch,
+//   ArgumentsHost,
+//   HttpException,
+//   HttpStatus,
+// } from '@nestjs/common';
+// //import { __ } from '@squareboat/nestjs-localization/dist/src';
+// import { Response, Request } from 'express';
 
-    //console.log('current language', i18n.lang);
-    const statusCode =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-    let message = exception.message;
-    if (message) {
-      const lang = request.headers['accept-language'] || 'en';
-      //const trans_message = __(message, lang);
-      if (message.includes('ERROR')) {
-        message = exception.message;
-      } else {
-        //message = trans_message;
-        message = exception.message;
-      }
-    } else {
-      message = 'Internal server error';
-    }
-    
-    console.log('RESPONSE EXP:::');
-    
-    response.status(statusCode).json({
-      statusCode,
-      message,
-      data:null
-    });
-  }
-}
-*/
+// @Catch(HttpException)
+// export class AllExceptionsFilter implements ExceptionFilter {
+//   async catch(exception: HttpException, host: ArgumentsHost) {
+//     const ctx = host.switchToHttp();
+//     const context = host.switchToHttp();
+//     const response = context.getResponse<Response>();
+//     const request = ctx.getRequest<Request>();
+//     const statusCode =
+//       exception instanceof HttpException
+//         ? exception.getStatus()
+//         : HttpStatus.INTERNAL_SERVER_ERROR;
+//     const message = exception['response'].message || exception.message || 'Internal server error';
+//     const data = null;
+
+//     response.status(statusCode).json({
+//       statusCode,
+//       message,
+//       data
+//     });
+//   }
+// }
